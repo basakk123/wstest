3b328bcaa1c681a38405dfa08346b4e05ac7a846
basakk123 <basakk@naver.com>
2022-09-30 16:50:12 +0900

    채팅 기본 기능 완료

---
+++ src/main/java/site/metacoding/red/WebsocketchatApplication.java
@@ -0,0 +1,11 @@
+package site.metacoding.red;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketchatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketchatApplication.class, args);
+    }
+}
---
+++ src/main/java/site/metacoding/red/web/ChatController.java
@@ -0,0 +1,31 @@
+package site.metacoding.red.web;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.red.service.ChatService;
+import site.metacoding.red.web.dto.request.chat.ChatRoom;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
